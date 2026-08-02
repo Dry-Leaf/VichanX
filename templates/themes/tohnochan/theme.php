895a2a89c105d5ac7f11262d130689f1ffb3df7b
n/a <n/a>
2026-08-02 11:21:29 -0400

    recent posts, get last 50 link

--- templates/themes/tohnochan/theme.php
+++ templates/themes/tohnochan/theme.php
@@ -155,19 +155,12 @@ class index
 
             if ($post["thread"]) {
                 $config['noko50_count'];
-
-                error_log(print_r(sprintf(
-                    "SELECT (COUNT(*) > `%d`) FROM ``posts_%s`` WHERE `thread` = :thread",
-                    $config['noko50_count'],
-                    $_board["uri"]
-                ), TRUE));
-
                 $countQuery = prepare(sprintf(
-                    "SELECT (COUNT(*) > `%d`) FROM ``posts_%s`` WHERE `thread` = :thread",
+                    "SELECT (COUNT(*) > %d) FROM ``posts_%s`` WHERE `thread` = :thread",
                     $config['noko50_count'],
                     $_board["uri"]
                 ));
-                $countQuery->bindValue(':thread', $$post["thread"], PDO::PARAM_INT);
+                $countQuery->bindValue(':thread', $post["thread"], PDO::PARAM_INT);
                 $countQuery->execute();
                 $last50 = $countQuery->fetch(PDO::FETCH_ASSOC);
             }
