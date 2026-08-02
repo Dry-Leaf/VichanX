450f02b71fadc9f3dbe2ff809662ddc9c00e7bf8
n/a <n/a>
2026-08-02 12:55:27 -0400

    recent posts, get last 50 link

--- templates/themes/tohnochan/theme.php
+++ templates/themes/tohnochan/theme.php
@@ -166,8 +166,8 @@ class index
                 ));
                 $countQuery->bindValue(':thread', $post["thread"], PDO::PARAM_INT);
                 $countQuery->execute();
-                $post["snippet"] = ((int)$countQuery->fetchColumn() >= $config['noko50_min'] - 1);
-                $last50 = ((int)$countQuery->fetchColumn() >= $config['noko50_min'] - 1);
+                $post["snippet"] = $countQuery->fetchColumn() >= $config['noko50_min'] - 1;
+                $last50 = $countQuery->fetchColumn() >= $config['noko50_min'] - 1;
             }
 
             $post["link"] =
